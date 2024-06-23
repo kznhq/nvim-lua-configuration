bf392c11b5584c62cb9e6785a6d01b3683299343
kznhq <145292978+kznhq@users.noreply.github.com>
2024-06-23 16:32:45 -0500

    changed the harpoon navigation keys to vim movement keys, also added maps to move between errors from lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,2 +1,5 @@
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.lua_ls.setup{}
+vim.keymap.set("n", "<leader>ne", function() vim.diagnostic.goto_next() end, opts)
+vim.keymap.set("n", "<leader>pe", function() vim.diagnostic.goto_prev() end, opts)
+vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
