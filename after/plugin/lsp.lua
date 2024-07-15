f94e4cf54632ef9d05737df56ef1db9058213dfa
Kaizan Haque <kaizanhaque@gmail.com>
2024-07-14 20:45:11 -0500

    added some more languages to treesitter and also added the tsserver LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,6 @@
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.lua_ls.setup{}
+require'lspconfig'.tsserver.setup{}
 vim.keymap.set("n", "<leader>ne", function() vim.diagnostic.goto_next() end, opts)
 vim.keymap.set("n", "<leader>pe", function() vim.diagnostic.goto_prev() end, opts)
 vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
