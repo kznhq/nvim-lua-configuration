f7c43078dae0247abef676a5d66f5c8fde00a663
Kaizan Haque <kaizanhaque@gmail.com>
2025-06-30 21:22:37 -0500

    changed init.lua so color scheme is translucent with kanagawa dragon

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
 require("required4lua")
+require("kanagawa").setup({ --remove this and the vim.cmd if you don't want transparent (background is dark brown)
+    theme = "dragon",
+    transparent = true,
+})
+
+vim.cmd("colorscheme kanagawa")
