2e8b134b15eca28f603cc115a9c2f45cfd4022db
= <kaizanhaque@gmail.com>
2025-07-19 16:56:18 -0500

    changed color scheme to kanagawa paper

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "kanagawa-dragon"
+	color = color or "kanagawa-paper"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
